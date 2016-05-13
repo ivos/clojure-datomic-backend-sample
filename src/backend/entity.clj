be413d9876ee01b497b9959cbbea2a5f23d915ab
Ivo Maixner <ivo.maixner@gmail.com>
2016-05-13 14:28:23 +0200

    Add return result from project-create.

--- src/backend/entity.clj
+++ src/backend/entity.clj
@@ -8,12 +8,25 @@
   (let [namespaced (keyword (name ns) (get data attribute))]
     (assoc data attribute namespaced)))
 
+
+(defn strip-value-ns
+  [data attribute]
+  (->> attribute
+    (get data)
+    name
+    keyword
+    (assoc data attribute)))
+
 (defn ns-keys
   [data attributes]
   (let [name->attr (zipmap (map name attributes) attributes)
         map-key #(if-some [mapped (-> % name name->attr)] mapped %)
         key-attrs (map map-key (keys data))]
     (zipmap key-attrs (vals data))))
+
+(defn strip-keys-ns
+  [data]
+  (zipmap (map name (keys data)) (vals data)))
 
 (defn- attribute-add-tx
   [data attribute]
@@ -22,6 +35,6 @@
 
 (defn entity-create-tx
   [db-partition attributes data]
-  (let [data-with-defaults (assoc data :id (d/tempid db-partition) :entity/version 1)
+  (let [data-with-defaults (assoc data :entity/version 1)
         add-txs (map (partial attribute-add-tx data-with-defaults) (conj attributes :entity/version))]
     (filter identity add-txs)))
--- src/backend/project.clj
+++ src/backend/project.clj
@@ -4,21 +4,29 @@
             [backend.entity :refer :all]
             ))
 
-(def ^:private db-partition :backend)
+(def ^:private db-partition :db.part/backend)
 (def ^:private attributes [:project/name :project/code :project/visibility])
 
 (defn project-create
   [request]
   (let [conn (:connection request)
+        tempid (d/tempid db-partition -1)
         data (-> (:body request)
                (ns-keys attributes)
-               (ns-value :project/visibility :project.visibility))
-        _ (log/debug "Create project, data" data)
+               (ns-value :project/visibility :project.visibility)
+               (assoc :id tempid))
+        _ (log/info "Create project" data)
         tx (entity-create-tx db-partition attributes data)
-        _ (log/debug "Tx" tx)
-        tx-result (d/transact conn tx)
-        _ (log/debug "Tx result" tx-result)
-        _ (log/debug "Resolved tempids" (:tempids tx-result))])
-  {:status 200
-   :headers {"Content-Type" "text/html; charset=utf-8"}
-   :body (str "<h1>Created</h1><p>" request "</p>")})
+        _ (log/trace "Tx" tx)
+        tx-result @(d/transact conn tx)
+        _ (log/trace "Tx result" tx-result)
+        id (d/resolve-tempid (d/db conn) (:tempids tx-result) tempid)
+        saved (merge {:db/id id} (d/touch (d/entity (d/db conn) id)))
+        _ (log/debug "Saved" saved)
+        result (-> saved
+                 (strip-value-ns :project/visibility)
+                 strip-keys-ns)
+        _ (log/info "Result" result)
+        ]
+    {:status 200
+     :body result}))

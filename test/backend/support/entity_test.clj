fcefb2dadd4ade0997e42ebe775456cf27856f14
Ivo Maixner <ivo.maixner@gmail.com>
2016-06-14 09:41:25 +0200

    Switch to midje.

--- test/backend/support/entity_test.clj
+++ test/backend/support/entity_test.clj
@@ -1,126 +1,116 @@
 (ns backend.support.entity-test
-  (:require [clojure.test :refer :all]
-            [slingshot.slingshot :refer [try+]]
+  (:require [slingshot.slingshot :refer [try+]]
+            [midje.sweet :refer :all]
             [backend.support.entity :refer :all]))
 
-(deftest empty-strings-to-nils-test
-  (testing
-    (let [data {:with-value "abc"
-                :empty-string ""
-                :number-zero 0
-                :false-bool false
-                :number-non-zero 123
-                :keyword :keyword-value}]
-      (is (= (empty-strings-to-nils data)
-             {:with-value "abc"
-              :empty-string nil
+(facts
+  "empty-strings-to-nils"
+  (let [data {:with-value "abc"
+              :empty-string ""
               :number-zero 0
               :false-bool false
               :number-non-zero 123
-              :keyword :keyword-value}
-             ))))
+              :keyword :keyword-value}]
+    (empty-strings-to-nils data)
+      => {:with-value "abc"
+       :empty-string nil
+       :number-zero 0
+       :false-bool false
+       :number-non-zero 123
+       :keyword :keyword-value})
   )
 
-(deftest remove-nil-values-test
-  (testing
-    (let [data {:with-value "abc"
-                :nil-value1 nil
-                :number-zero 0
-                :false-bool false
-                :nil-value2 nil
-                :number-non-zero 123
-                :keyword :keyword-value}]
-      (is (= (remove-nil-values data)
-             {:with-value "abc"
+(facts
+  "remove-nil-values"
+  (let [data {:with-value "abc"
+              :nil-value1 nil
               :number-zero 0
               :false-bool false
+              :nil-value2 nil
               :number-non-zero 123
-              :keyword :keyword-value}
-             ))))
+              :keyword :keyword-value}]
+    (remove-nil-values data)
+    => {:with-value "abc"
+        :number-zero 0
+        :false-bool false
+        :number-non-zero 123
+        :keyword :keyword-value})
   )
 
-(deftest ensure-all-attributes-test
-  (testing
-    (let [data {:with-value "abc"
-                :nil-value1 nil
-                :false-bool false
-                :number-non-zero 123
-                :other "other-value"
-                :keyword :keyword-value}
-          attributes [:with-value :nil-value1 :number-non-zero :missing1 :missing2 :keyword]]
-      (is (= (ensure-all-attributes data attributes)
-             {:with-value "abc"
+(facts
+  "ensure-all-attributes"
+  (let [data {:with-value "abc"
               :nil-value1 nil
               :false-bool false
               :number-non-zero 123
               :other "other-value"
-              :keyword :keyword-value
-              :missing1 nil
-              :missing2 nil}
-             ))))
+              :keyword :keyword-value}
+        attributes [:with-value :nil-value1 :number-non-zero :missing1 :missing2 :keyword]
+        ]
+    (ensure-all-attributes data attributes)
+    => {:with-value "abc"
+        :nil-value1 nil
+        :false-bool false
+        :number-non-zero 123
+        :other "other-value"
+        :keyword :keyword-value
+        :missing1 nil
+        :missing2 nil})
   )
 
-(deftest ns-value-test
-  (testing
-    "Ok."
+(facts
+  "ns-value"
+  (fact
+    "Ok"
     (let [data {:a 1 :an-attr "a-value"}]
-      (is (= {:a 1 :an-attr :a-ns/a-value}
-             (ns-value data :an-attr :a-ns)))))
-  (testing
-    "Attribute value is nil."
+      (ns-value data :an-attr :a-ns) => {:a 1 :an-attr :a-ns/a-value}))
+  (fact
+    "Attribute value is nil"
     (let [data {:a 1 :an-attr nil}]
-      (is (= {:a 1 :an-attr nil}
-             (ns-value data :an-attr :a-ns)))))
-  (testing
-    "Attribute is not present."
+      (ns-value data :an-attr :a-ns) => {:a 1 :an-attr nil}))
+  (fact
+    "Attribute is not present"
     (let [data {:a 1}]
-      (is (= {:a 1}
-             (ns-value data :an-attr :a-ns)))))
+      (ns-value data :an-attr :a-ns) => {:a 1}))
   )
 
-(deftest ns-keys-test
-  (testing
-    "Found is replaced."
-    (is (= 
-          {:my-entity/attr 1}
-          (ns-keys {:attr 1} [:my-entity/attr]))))
-  (testing
-    "Not found is kept intact."
-    (is (= 
-          {:non-existing 1}
-          (ns-keys {:non-existing 1} [:my-entity/attr]))))
-  (testing
-    "Multiple namespaces."
-    (is (= 
-          {:my-entity/attr-a 1 :other-entity/attr-b 2}
-          (ns-keys {:attr-a 1 :attr-b 2} [:my-entity/attr-a :other-entity/attr-b]))))
-  (testing
-    "Nil value."
-    (is (= 
-          {:my-entity/attr nil}
-          (ns-keys {:attr nil} [:my-entity/attr]))))
+(facts
+  "ns-keys"
+  (fact
+    "Found is replaced"
+    (ns-keys {:attr 1} [:my-entity/attr]) => {:my-entity/attr 1})
+  (fact
+    "Not found is kept intact"
+    (ns-keys {:non-existing 1} [:my-entity/attr]) => {:non-existing 1})
+  (fact
+    "Multiple namespaces"
+    (ns-keys {:attr-a 1 :attr-b 2} [:my-entity/attr-a :other-entity/attr-b])
+    => {:my-entity/attr-a 1 :other-entity/attr-b 2})
+  (fact
+    "Nil value"
+    (ns-keys {:attr nil} [:my-entity/attr]) => {:my-entity/attr nil})
   )
 
-(deftest entity-create-tx-test
-  (testing
-    (let [attributes [:add1 :add2 :keep-nil3]
-          data {:eid :v-eid
-                :add1 :v-add1
-                :add2 :v-add2
-                :other :v-other}
-          tx (entity-create-tx :db-part1 :type1 attributes data)
-          ]
-      (is (=
-            '([:db/add :v-eid :add1 :v-add1]
-               [:db/add :v-eid :add2 :v-add2]
-               [:db/add :v-eid :entity/version 1]
-               [:db/add :v-eid :entity/type :type1])
-            tx))
-      ))
+(facts
+  "entity-create-tx"
+  (let [attributes [:add1 :add2 :keep-nil3]
+        data {:eid :v-eid
+              :add1 :v-add1
+              :add2 :v-add2
+              :other :v-other}
+        ]
+    (entity-create-tx :db-part1 :type1 attributes data)
+    => '([:db/add :v-eid :add1 :v-add1]
+          [:db/add :v-eid :add2 :v-add2]
+          [:db/add :v-eid :entity/version 1]
+          [:db/add :v-eid :entity/type :type1])
+    )
   )
 
-(deftest entity-update-tx-test
-  (testing
+(facts
+  "entity-update-tx"
+  (fact
+    "Ok"
     (let [attributes [:modify1 :modify2 :retract3 :add4 :keep-same5 :keep-nil6]
           db-data {
                    :modify1 :db-modify1
@@ -137,19 +127,17 @@
                 :keep-same5 :v-keep-same5
                 :keep-nil6 nil
                 :other :v-other}
-          tx (entity-update-tx :db-part1 :type1 attributes db-data data 123)
           ]
-      (is (=
-            '([:optimistic-lock :v-eid 123]
-               [:db/add :v-eid :entity/version 124]
-               [:db/add :v-eid :modify1 :v-modify1]
-               [:db/add :v-eid :modify2 :v-modify2]
-               [:db/retract :v-eid :retract3 :db-retract3]
-               [:db/add :v-eid :add4 :v-add4]
-               )
-            tx))
+      (entity-update-tx :db-part1 :type1 attributes db-data data 123)
+      => '([:optimistic-lock :v-eid 123]
+            [:db/add :v-eid :entity/version 124]
+            [:db/add :v-eid :modify1 :v-modify1]
+            [:db/add :v-eid :modify2 :v-modify2]
+            [:db/retract :v-eid :retract3 :db-retract3]
+            [:db/add :v-eid :add4 :v-add4]
+            )
       ))
-  (testing
+  (facts
     "Invalid version"
     (let [attributes [:a]
           db-data {:a :db-a
@@ -159,30 +147,32 @@
           ]
       (try+ (do
               (entity-update-tx :db-part1 :type1 attributes db-data data "invalid")
-              (is false "Should throw"))
+              (fact "Should throw"
+                    true => false))
             (catch [:type :optimistic-locking-failure] {:keys [:v]}
-              (is (= :db-version v))))
+              v => :db-version))
       ))
   )
 
-(deftest entity-delete-tx-test
-  (testing
+(facts
+  "entity-delete-tx"
+  (facts
+    "Ok"
     (let [db-data {:entity/version :db-version}
           tx (entity-delete-tx db-data 678 123)
           ]
-      (is (=
-            '([:optimistic-lock 678 123]
+      tx => '([:optimistic-lock 678 123]
                [:db.fn/retractEntity 678]
                )
-            tx))
       ))
-  (testing
+  (facts
     "Invalid version"
     (let [db-data {:entity/version :db-version}]
       (try+ (do
               (entity-delete-tx db-data 678 "invalid")
-              (is false "Should throw"))
-            (catch [:type :optimistic-locking-failure] {:keys [:v]}
-              (is (= :db-version v)))))
-    )
+              (fact "Should throw"
+                    true => false))
+              (catch [:type :optimistic-locking-failure] {:keys [:v]}
+                v => :db-version))
+      ))
   )
--- test/backend/support/validation_test.clj
+++ test/backend/support/validation_test.clj
@@ -1,26 +1,26 @@
 (ns backend.support.validation-test
-  (:require [clojure.test :refer :all]
-            [slingshot.slingshot :refer [try+]]
-            [backend.support.validation :refer :all]))
+  (:require [slingshot.slingshot :refer [try+]]
+            [backend.support.validation :refer :all]
+            [midje.sweet :refer :all]))
 
-(deftest verify-keys!-test
-  (testing
-    "Ok."
+(facts
+  "verify-keys!"
+  (facts
+    "Ok"
     (let [attributes [:a :b :c :d]
           data {:a 1 :b 2 :d 4}]
-      (is (=
-            nil
-            (verify-keys! attributes data)))))
-  (testing
+      (verify-keys! attributes data) => nil
+      ))
+  (facts
     "Invalid attributes"
     (let [attributes [:a :b :c :d]
           data {:a 1 :b 2 :e 4 :f 5}]
       (try+ (do
               (verify-keys! attributes data)
-              (is false "Should throw"))
+              (fact "Should throw"
+                    true => false))
         (catch [:type :backend.support.validation/validation-failure] {:keys [errors]}
-          (is (= errors
-                 {:e ["invalid.attribute"] :f ["invalid.attribute"]})))
+          errors => {:e ["invalid.attribute"] :f ["invalid.attribute"]})
         ))
     )
   )
